e82b860fa404c5b675c589c28da43431ae064426
joaolucasdemelo <joaolucas.melo1619@gmail.com>
2023-12-19 03:21:57 -0300

    Projeto finalizado

--- src/index.js
+++ src/index.js
@@ -1,2 +1,37 @@
-let nome = "felipe";
-console.log(nome);
+//Variáveis
+//Heróis
+let Heroi0 = "Paladino Sagrado";
+let Heroi1 = "Cavaleiro Mesquinho";
+let Heroi2 = "Samurai Oden de Wano";
+let Heroi3 = "Espadachim Myamoto Musashi";
+let Heroi4 = "Mago Gandalf o Cinzento";  
+let Heroi5 = "Monkey D. Luffy";  
+
+//Xp e Ranked
+let Heroi0Nivel = "Ouro"
+let Heroi1Nivel = "Ferro"
+let Heroi2Nivel = "Ascendente"
+let Heroi3Nivel = "Imortal"
+let Heroi4Nivel = "Platina"
+let Heroi5Nivel = "Rei"
+
+console.log("O Herói de nome " + Heroi3 + " está no nível de " + Heroi4Nivel)
+
+//Laços de Repetição
+for (let FerroXp = 0; FerroXp < 1000;)
+
+for (let OuroXp = 6001; OuroXp < 7000;)
+
+for (let PlatinaXp = 7001; PlatinaXp < 8000;)
+
+for (let AscendenteXp = 8001; AscendenteXp <9000;)
+
+for (let ImortalXp = 9001; ImortalXp <10000;)
+
+for (let ReiXp = 10001; ReiXp <20000;){
+
+} 
+
+// Desculpa professor Felipão, eu entendi bem as outras explicações sobre Operadores e Estruturas de decisões, porém não consegui entender uma forma de "encaixar" isso com as informações dos Heróis! 
+
+
